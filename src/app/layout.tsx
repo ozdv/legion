5a3d43ffb8e3d8458f65546aba3d30a37e10532f
ozdv <cam@ozdv.me>
2024-01-29 13:40:44 -0700

    components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       <body className={inter.className}>{children}</body>
     </html>
   );
