4db1fd0f77d892cbc01097b3faa9880729f11b01
ozdv <cam@ozdv.me>
2024-01-30 08:26:25 -0700

    build error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/button";
 import { Container } from "@/components/container";
 import { Layout } from "@/components/layout";
 
@@ -15,9 +16,9 @@ export default function NotFound() {
           <p className="mt-4 text-lg tracking-tight text-gray-300">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
-          {/* <Button href="/" className="mt-8">
+          <Button href="/" className="mt-8">
             Go back home
-          </Button> */}
+          </Button>
         </Container>
       </div>
     </Layout>
