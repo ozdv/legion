41d9b5cc82552f0a3762095a329dd3e5cd9f8760
ozdv <cam@ozdv.me>
2024-02-24 08:00:22 -0700

    wups

--- src/components/hero/description.tsx
+++ src/components/hero/description.tsx
@@ -10,12 +10,9 @@ const Description = (props: Props) => {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl sm:text-center">
           <h2 className="text-3xl font-medium tracking-tight text-gray-900">
-            Now is the time to build your portfolio.
+            Who are we?
           </h2>
-          <p className="mt-2 text-lg text-gray-600">
-            With typical market returns, you have to start young to secure your
-            future. With Pocket, it’s never too late to build your nest egg.
-          </p>
+
           <p className="mt-2 text-lg text-gray-600">
             The Legion of Mary is a Catholic apostolic association with the
             sanction of the Church, and under the powerful leadership of Mary
